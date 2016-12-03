35fae2cfed8126948850e526f675568663617d78
_almi_n <almin.yv@gmail.com>
2016-12-03 16:36:53 +0300

    v1.1.1

---
+++ minrs/js/examples.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    var slider_1    = new MINRS.Slider('rsSlider-1', {
+        dual: false,
+        end: 400,
+        start: 0,
+        min: 0,
+        max: 0
+    });
+};
